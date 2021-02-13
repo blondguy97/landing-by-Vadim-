b27e4a586f1b2aca9ec1ccd4e4d71fa0a6aafe03
Yasha <yasha.avramov@yandex.ru>
2021-02-13 16:58:32 +0300

    with adaptive

--- js/main.js
+++ js/main.js
@@ -5,7 +5,23 @@ $(function () {
         slidesToScroll: 2,
         infinite: true,
         arrows: false,
-        dots: true
+        dots: true,
+        responsive: [
+            {
+              breakpoint: 1106,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+            }
+          ]
     });
+
+});
+
+
+
+$('.btn__menu').on('click', function() {
+    $('.menu__list').slideToggle();
 
 });
